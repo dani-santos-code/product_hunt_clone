2ba3657c57f4a59120d2e41b4933825ee2942f4a
Daniele de Araujo dos Santos <danisantos@Danieles-MacBook-Pro-2.local>
2018-11-12 13:31:24 +0100

    add finishing touches

--- products/views.py
+++ products/views.py
@@ -4,7 +4,8 @@ from .models import Product
 from django.utils import timezone
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html', {'products': products})
 
 @login_required
 def create(request):
@@ -29,6 +30,15 @@ def create(request):
     else:
         return render(request, 'products/create.html')
 
+@login_required(login_url="/accounts/signup")
 def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html', {'product': product})
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method =="POST":
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total+= 1
+        product.save()
+        return redirect('/products/' + str(product.id))
