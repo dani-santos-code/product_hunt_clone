c754c72457a6443d6e0dbb2d908247eff4a70ede
Daniele de Araujo dos Santos <danisantos@Danieles-MacBook-Pro-2.local>
2018-11-12 10:37:09 +0100

    Add the auth stuff

---
+++ accounts/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+def signup(request):
+    if request.method == "POST":
+        #the user wants to sign up
+        if request.POST['password1'] == request.POST['password2']:
+                try:
+                #looking for similar usernames already in the User object
+                    user = User.objects.get(username=request.POST['username'])
+                    return render(request, 'accounts/signup.html', {'error1': 'Username has already been taken'})
+                except User.DoesNotExist:
+                    user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                    auth.login(request, user)
+                    return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error2': 'Passwords don\'t match! :('})
+    else:
+        #user wants to return info
+        return render(request, 'accounts/signup.html')
+
+def login(request):
+    if request.method == "POST":
+        #the user wants to log in
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Ooops! Something went wrong.'})
+
+    else:
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    #TODO need to route into homepage
+    #and don't forget to logout
+    if request.method == "POST":
+        auth.logout(request)
+        return redirect('home')
+    else:
+        return render(request, 'accounts/signup.html')
